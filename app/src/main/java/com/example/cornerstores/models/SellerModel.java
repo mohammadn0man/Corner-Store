4bbc2cb7d700185715932f242025c9844762b703
Rahul Dhiman <9580rahuldhiman@gmail.com>
2020-03-17 22:37:48 +0530

    addition of com.example.cornerstores.models Package 1.0

--- app/src/main/java/com/example/cornerstores/models/SellerModel.java
+++ app/src/main/java/com/example/cornerstores/models/SellerModel.java
@@ -2,6 +2,6 @@ package com.example.cornerstores.models;
 
 public class SellerModel extends OrderModel{
     public int sale_id;
-    public float price;
+    public float amount_sold;
     public String date_of_sale;
 }
